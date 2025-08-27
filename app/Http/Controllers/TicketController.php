546eeaa8cb2eb182c39bf8c51815998e426dc20f
Daca.dev <daca.dev97@gmail.com>
2025-08-26 19:24:43 -0500

    Agregar controlador y modelo de Ticket, junto con la migración para la tabla de tickets

---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Ticket;
+
+class TicketController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // select * from tickets
+        return response()->json(Ticket::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('tickets.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate(
+            [
+                'user_id' => 'required|exists:users,id',
+                'title' => 'required|string|max:255',
+                'description' => 'required|string',
+                'status' => 'in:open,in_progress,closed',
+            ],
+            [
+                'user_id.required' => 'El usuario es obligatorio',
+                'user_id.exists' => 'El usuario ya existe',
+            ]
+        );
+
+        $ticket = new Ticket($validated);
+        $ticket->save();
+
+        return response()->json($ticket, 201)->with('message', 'Ticket created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id) {}
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
